80fc00dd43fe38934acee4bc8a8689677bc3ad50
Alejandro Martí <alejandromarti@lingokids.com>
2024-11-18 14:19:02 +0100

    Basic request

---
+++ catorrasso.py
@@ -0,0 +1,37 @@
+import requests
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+
+# URL
+url = "https://api.futmondo.com/1/userteam/roster"
+
+# Payload with a header field and a query field
+data = {
+    "header": {
+        "token": ACCESS_TOKEN,
+        "userid": "64aeca5fa4b22c0939d64c9b"
+    },
+    "query": {
+        "championshipId":"5b51f25a2a0776bf08db8a10",
+        "userteamId": "64aecaef5b1a1779b5d0c40a"
+    }
+} 
+
+# Headers (generic for JSON content)
+headers = {
+    "Content-Type": "application/json"
+}
+
+# Make the POST request
+response = requests.post(url, headers=headers, json=data)
+
+# Check the response
+if response.status_code == 200:
+    print("Request successful:", response.json())
+else:
+    print("Request failed with status code:", response.status_code)
+    print("Response:", response.text)
